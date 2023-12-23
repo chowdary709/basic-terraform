6019131e6af2fd687cfe5a082ca2610c47e0bcb9
hari chandra prasad chowdary chandra <chandrahari321@gmail.com>
2023-12-23 06:11:33 +0530

    automated commit #218

---
+++ vars/vars.tf
@@ -0,0 +1,27 @@
+variable "fruit" {
+  default = "apple"
+}
+
+variable "fruits" {
+  default = ["apple","banana"]
+}
+
+variable "fruits_price" {
+  default = {
+    apple = 10
+    banana = 20
+  }
+}
+
+output "fruit" {
+  value = var.fruit
+}
+
+output "fruits" {
+  value = var.fruits
+}
+
+output "fruits_price"{
+  value = var.fruits_price
+}
+
